b3ab4a193efc67a06db4f000ce45de98618a67a8
Lautaro Robles <robleslautaro54@gmail.com>
2025-10-28 16:45:40 -0300

    controladorProveedores

---
+++ src/main/java/controladores/controladorProveedor.java
@@ -0,0 +1,110 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSF/JSFManagedBean.java to edit this template
+ */
+package controladores;
+
+import entidades.Proveedor;
+import jakarta.annotation.PostConstruct;
+import jakarta.inject.Named;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.enterprise.inject.Model;
+import jakarta.enterprise.inject.Produces;
+import jakarta.inject.Inject;
+import java.util.List;
+import repositorios.repoProveedor;
+
+/**
+ *
+ * @author roble
+ */
+@Named(value = "controladorProveedor")
+@RequestScoped
+public class controladorProveedor {
+    
+    @Inject
+    private repoProveedor repoProveedor;
+    
+    private Proveedor proveedor;
+    
+    private Integer id;
+    
+    public controladorProveedor() {
+    }
+    
+    @Model
+    @Produces
+    public Proveedor proveedor() {
+        System.out.println("id" + id);
+        if (id != null && id > 0) {
+            repoProveedor.porId(id).ifPresent(p -> {
+                proveedor = p;
+            });
+        }else{
+            proveedor = new Proveedor();
+        }
+        return proveedor;
+    }
+    
+    /*
+    @PostConstruct
+    public void init() {
+        proveedor = new Proveedor();
+        
+        proveedor.setEstado(true);
+    }*/
+    
+    public List<Proveedor> listar() {
+        return repoProveedor.Listar();
+    }
+
+    public String guardar() {
+        repoProveedor.Guardar(proveedor);
+        return "/proveedores/index.xhtml?faces-redirect=true";
+    }
+
+    public String eliminar(Integer id) {
+        repoProveedor.Eliminar(id);
+        return "/proveedores/index.xhtml?faces-redirect=true";
+    }
+    
+    public String bajaLogica(Integer id) {
+        repoProveedor.BajaLogica(id);
+        return "/proveedores/index.xhtml?faces-redirect=true";
+    }
+    
+    public String reactivar(Integer id) {
+        repoProveedor.Reactivar(id);
+        return "/proveedores/index.xhtml?faces-redirect=true";
+    }
+
+    public repoProveedor getRepoProveedor() {
+        return repoProveedor;
+    }
+
+    public void setRepoProveedor(repoProveedor repoProveedor) {
+        this.repoProveedor = repoProveedor;
+    }
+
+    public Proveedor getProveedor() {
+        if (proveedor == null) {
+            proveedor();
+        }
+        return proveedor;
+    }
+
+    public void setProveedor(Proveedor proveedor) {
+        this.proveedor = proveedor;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    
+    
+    
+}
