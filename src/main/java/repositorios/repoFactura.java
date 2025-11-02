279b4eae07b06159f93b1fdf1727edc0a99b6e0e
Lautaro Robles <robleslautaro54@gmail.com>
2025-11-02 15:52:07 -0300

    Facturas

--- src/main/java/repositorios/repoFactura.java
+++ src/main/java/repositorios/repoFactura.java
@@ -4,10 +4,56 @@
  */
 package repositorios;
 
+import entidades.Factura;
+import jakarta.ejb.Stateless;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
 /**
  *
  * @author roble
  */
+@Stateless
 public class repoFactura {
+    @Inject
+    EntityManager em;
+    
+     public void Guardar(Factura f) {
+        if (f.getIdFactura() != null && f.getIdFactura() > 0) {
+            em.merge(f);
+        } else {
+            em.persist(f);
+        }
+    }
+/*
+    public void BajaLogica(Integer id) {
+        porId(id).ifPresent(f -> {
+            f.setEstado(false);
+            em.merge(f);
+        });
+    }
+    
+    public void Reactivar(Integer id) {
+        porId(id).ifPresent(f -> {
+            f.setEstado(true);
+            em.merge(f);
+        });
+    }
+*/
+    public void Eliminar(Integer id) {
+        porId(id).ifPresent(p -> {
+            em.remove(p);
+        });
+    }
+
+    public Optional<Factura> porId(Integer id) {
+        return Optional.ofNullable(em.find(Factura.class, id));
+    }
+
+    public List<Factura> Listar() {
+        return em.createQuery("SELECT f FROM Factura f", Factura.class).getResultList();
+    }
     
 }
