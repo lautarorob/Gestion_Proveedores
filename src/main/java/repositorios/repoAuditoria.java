f2eeb3b42336fbaac2390d7fa4a9fc74b63a31d2
Lautaro Robles <robleslautaro54@gmail.com>
2025-11-21 11:53:08 -0300

    Auditoria y Dashboard

--- src/main/java/repositorios/repoAuditoria.java
+++ src/main/java/repositorios/repoAuditoria.java
@@ -4,10 +4,35 @@
  */
 package repositorios;
 
+import entidades.Auditoria;
+
+import jakarta.ejb.Stateless;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import java.util.List;
+
 /**
  *
  * @author roble
  */
+@Stateless
 public class repoAuditoria {
-    
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public List<Auditoria> Listar() {
+        System.out.println("Ejecutando consulta de Auditoria...");
+        return em.createQuery("SELECT a FROM Auditoria a", Auditoria.class)
+                .getResultList();
+    }
+
+    public void Guardar(Auditoria u) {
+        if (u.getIdAuditoria() != null && u.getIdAuditoria() > 0) {
+            em.merge(u);
+        } else {
+            em.persist(u);
+        }
+    }
 }
